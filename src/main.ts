29ac845188a6305193acee2d0984afd18ba10f23
Dmitriy Khutornoy <thth13q@gmail.com>
2025-01-02 15:45:33 +0200

    swagger init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { BookSummaryModule } from './book-summary/book-summary.module';
+import { ProfileModule } from './profile/profile.module';
+import { UserModule } from './user/user.module';
+import { BookModule } from './book/book.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const options = new DocumentBuilder()
+    .setTitle('API')
+    .setDescription('API description')
+    .setVersion('1.0')
+    .addTag('API')
+    .build();
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [BookSummaryModule, BookModule, ProfileModule, UserModule],
+  });
+  SwaggerModule.setup('api', app, document);
 
   const PORT = process.env.PORT || 3000;
   await app.listen(PORT);
