5aa35f78bb6639bd4b75b34510a81196c1dace11
Dmitriy Khutornoy <thth13q@gmail.com>
2025-05-18 02:35:39 +0300

    getBook try catch

--- src/book/book.service.ts
+++ src/book/book.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, BadRequestException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import axios from 'axios';
@@ -12,25 +12,29 @@ export class BookService {
   constructor(@InjectModel('Book') private readonly bookModel: Model<BookDocument>) {}
 
   async getBook(googleBookId: string): Promise<Book> {
-    const book = await this.bookModel.findOne({ googleId: googleBookId });
+    try {
+      const book = await this.bookModel.findOne({ googleId: googleBookId });
 
-    if (!book) {
-      const res = await axios.get(`${GOOGLE_BOOKS_API}/${googleBookId}/?key=AIzaSyC9nLTd3paExG1qsub80hlslKc3aydWJhw`);
+      if (!book) {
+        const res = await axios.get(`${GOOGLE_BOOKS_API}/${googleBookId}/?key=AIzaSyC9nLTd3paExG1qsub80hlslKc3aydWJhw`);
 
-      return await this.bookModel.create({
-        googleId: res.data.id,
-        title: res.data.volumeInfo.title,
-        description: res.data.volumeInfo.description,
-        authors: res.data.volumeInfo.authors,
-        imageLinks: res.data.volumeInfo.imageLinks,
-        categories: res.data.volumeInfo.categories,
-        publisher: res.data.volumeInfo.publisher,
-        publishedDate: res.data.volumeInfo.publishedDate,
-        pageCount: res.data.volumeInfo.pageCount,
-        language: res.data.volumeInfo.language,
-      });
+        return await this.bookModel.create({
+          googleId: res.data.id,
+          title: res.data.volumeInfo.title,
+          description: res.data.volumeInfo.description,
+          authors: res.data.volumeInfo.authors,
+          imageLinks: res.data.volumeInfo.imageLinks,
+          categories: res.data.volumeInfo.categories,
+          publisher: res.data.volumeInfo.publisher,
+          publishedDate: res.data.volumeInfo.publishedDate,
+          pageCount: res.data.volumeInfo.pageCount,
+          language: res.data.volumeInfo.language,
+        });
+      }
+
+      return book;
+    } catch (err) {
+      throw new BadRequestException('Bad request.');
     }
-
-    return book;
   }
 }
